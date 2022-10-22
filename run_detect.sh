5c681f6ff8bddcadc16ca3ca8c75c744e2e59d22
Moshe <moshesalz47@gmail.com>
2022-10-22 21:51:56 +0300

    Add all yolov5 detect facilities

---
+++ run_detect.sh
@@ -0,0 +1,2 @@
+python3 detect.py --weights best300.pt    --img 416 --conf 0.001  --source rtmp://localhost/live/camera2 --name camera1A --proj live 
+#python3 detect.py --weights best300.pt    --img 416 --conf 0.001  --source rtmp://localhost/live/camera3 --name camera3 --proj live &
